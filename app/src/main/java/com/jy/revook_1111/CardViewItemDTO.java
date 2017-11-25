69f3daddf101f86e7d20f09ea339a8438f28ef17
encia200 <encia200@gmail.com>
2017-11-26 06:03:55 +0900

    11260603

---
+++ app/src/main/java/com/jy/revook_1111/CardViewItemDTO.java
@@ -0,0 +1,13 @@
+package com.jy.revook_1111;
+
+class CardViewItemDTO {
+    int imageview;
+    String title;
+    String subtitle;
+
+    CardViewItemDTO(int imageview, String title, String subtitle) {
+        this.imageview = imageview;
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+}
--- app/src/main/java/com/jy/revook_1111/ReviewFragment.java
+++ app/src/main/java/com/jy/revook_1111/ReviewFragment.java
@@ -3,6 +3,8 @@ package com.jy.revook_1111;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,21 @@ import android.view.ViewGroup;
 public class ReviewFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_review, container, false);
+
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(getActivity());
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        RecyclerView rv = (RecyclerView) v.findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(mLinearLayoutManager);
+
+        //카드 리스트뷰 어댑터에 연결
+        ReviewRecyclerViewAdapter adapter = new ReviewRecyclerViewAdapter();
+        rv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        return v;
     }
 
 }
